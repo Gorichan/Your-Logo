cd77e8a68cf78b78e1d978abbd2dea7ddd5f5c18
Basss2ss <68554683+Basss2ss@users.noreply.github.com>
2020-07-24 16:09:50 +0300

    mob

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).reaby(function () {
+
+
+
+    $('ul.menu a[href^="#"').click(function () {
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('ul.menu a[href^="#"').css({
+            'color': '#212121'
+        });
+        $(this).css({
+            'color': '#004bee'
+        });
+        return false;
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0)
+        $('#toTop').fadeIn();
+        else
+        $('#toTop').fadeOut();
+    });
+    $('#toTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+    });
+
+    
+
+
+
+
+})
